589609723ef5e6fcceac366e4e4abb9cf7479d03
Thomas Wilgenbus <thomas.wilgenbus@happn.fr>
2020-05-28 21:33:14 +0200

    Extract gamma correction and clamping out of renderer

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,36 @@ use crate::collision::Collision;
 use crate::materials::lambertian_diffuse::random_unit_vector;
 use crate::rand_range_f64::rand_range_f64;
 use crate::ray::Ray;
+use crate::renderer::Color;
 use crate::shapes::shape::Shape;
 use crate::shapes::sphere::Sphere;
-use crate::renderer::Color;
 
 mod camera;
 mod collision;
 mod materials;
 mod rand_range_f64;
 mod ray;
-mod shapes;
 mod renderer;
+mod shapes;
 
-const SAMPLES_PER_PIXEL: i64 = 50;
+const SAMPLES_PER_PIXEL: i64 = 1;
 
 fn main_loop() {
     let camera = Camera::new();
-    let width = 1920.0;
-    let height = 1080.0;
+    let width = 640.0;
+    let height = 360.0;
 
     let sphere = Sphere::new(Vector3::new(0.0, 0.0, -1.0), 0.5);
     let sphere2 = Sphere::new(Vector3::new(0.0, -100.5, -1.0), 100.0);
     let sphere3 = Sphere::new(Vector3::new(0.5, -0.4, -0.85), 0.1);
     let scene: Vec<&dyn Shape> = vec![&sphere, &sphere2, &sphere3];
-    let mut renderer = renderer::RendererPPM::new(height as usize, width as usize, SAMPLES_PER_PIXEL);
+    let mut renderer = renderer::RendererPPM::new(height as usize, width as usize);
 
     eprint!("Scanlines remaining:\n");
     for y in (0..(height as i64)).rev() {
         eprint!("\r{} <= {}", height, height as i64 - y);
         for x in 0..(width as i64) {
-            let mut pixel_color = Vector3::new(0.0, 0.0, 0.0);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
             for _s in 0..SAMPLES_PER_PIXEL {
                 let offset_x = (x as f64 + rand_range_f64(0.0, 1.0)) / (width - 1.0);
                 let offset_y = (y as f64 + rand_range_f64(0.0, 1.0)) / (height - 1.0);
@@ -45,9 +45,14 @@ fn main_loop() {
                 pixel_color += r.project_ray(&scene);
             }
 
-            renderer.set_pixel(x as usize, y as usize, pixel_color);
+            let scale = 1.0 / SAMPLES_PER_PIXEL as f64;
+            let corrected_pixel_color = (pixel_color * scale)
+                .map(|c| c.clamp(0.0, 1.0))
+                .map(f64::sqrt);
+            renderer.set_pixel(x as usize, y as usize, corrected_pixel_color);
         }
     }
+
     eprint!("\nDone! :-)\n");
     renderer.render();
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,7 @@
 use nalgebra::Vector3;
+use std::borrow::{Borrow, BorrowMut};
+use std::io;
+use std::io::{BufWriter, Write};
 
 pub type Color = Vector3<f64>;
 
@@ -10,34 +13,35 @@ struct Size {
 pub struct RendererPPM {
     pixels: Vec<Color>,
     size: Size,
-    samples_per_pixel: i64,
 }
 
 impl RendererPPM {
-    pub fn new(height: usize, width: usize, samples_per_pixel: i64) -> Self {
+    pub fn new(height: usize, width: usize) -> Self {
         let count = width * height;
         let mut v = Vec::with_capacity(count);
-        v.resize_with(count, || Vector3::new(0.0,0.0,0.0));
+        v.resize_with(count, || Vector3::new(0.0, 0.0, 0.0));
         Self {
             pixels: v,
-            size: Size{width, height},
-            samples_per_pixel
+            size: Size { width, height },
         }
     }
     pub fn set_pixel(&mut self, x: usize, y: usize, color: Color) {
         self.pixels[y * self.size.width + x] = color;
     }
     pub fn render(&self) {
-        println!("P3\n{} {} \n255", self.size.width, self.size.height);
+        let stdout = io::stdout();
+        let mut writer = BufWriter::new(stdout.lock());
+        let header_lines = format!("P3\n{} {} \n255\n", self.size.width, self.size.height);
+        writer.write_all(header_lines.as_bytes());
+
         for y in (0..self.size.height).rev() {
             for x in 0..self.size.width {
                 let color = self.pixels[y * self.size.width + x];
-                
-                let scale = 1.0 / self.samples_per_pixel as f64;
-                let ir = (255.999 * (color[0] * scale).clamp(0.0, 1.0).sqrt()) as i64;
-                let ig = (255.999 * (color[1] * scale).clamp(0.0, 1.0).sqrt()) as i64;
-                let ib = (255.999 * (color[2] * scale).clamp(0.0, 1.0).sqrt()) as i64;
-                println!("{} {} {}", ir, ig, ib);
+
+                let ir = (255.999 * color[0]) as i64;
+                let ig = (255.999 * color[1]) as i64;
+                let ib = (255.999 * color[2]) as i64;
+                writer.write_all(format!("{} {} {}\n", ir, ig, ib).as_bytes());
             }
         }
     }
